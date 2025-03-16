9550a16a1527881dc1cf63dfc4a518f26046561f
Arthur VIENOT ONGARO <arthurvienot1008@gmail.com>
2025-03-16 16:41:10 +0100

    Set up Express server and create GET /random-quote endpoint

--- server.js
+++ server.js
@@ -1,10 +1,20 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-const { quotes } = require('./data');
-const { getRandomElement } = require('./utils');
+const { quotes } = require("./data");
+const { getRandomElement } = require("./utils");
 
 const PORT = process.env.PORT || 4001;
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 
+app.listen(PORT, () => {
+  console.log(`listening at port ${PORT}`);
+});
+
+app.get("/api/quotes/random", (req, res, next) => {
+  const random = getRandomElement(quotes);
+  res.send({
+    quote: { quotes },
+  });
+});
